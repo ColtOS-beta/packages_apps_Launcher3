5a0957c71b25d3a58e2fa5fb5eaf864a13474ebb
Ghosuto <clash.raja10@gmail.com>
2023-07-23 08:25:36 +0200

    Launcher3: Adapt for coltos
    Revert "Launcher3: Update AppLock API and integrate hidden apps"
    
    This reverts commit eec22f4d9f3c2c2d5f67217e5abe5478b4b739b8.
    
    Revert "Launcher3: Switch to AppLock API for protected apps"
    
    This reverts commit d28e70713c667a64c16c0aba87e763ad62c1a441.

--- src/com/android/launcher3/lineage/trust/TrustAppsActivity.java
+++ src/com/android/launcher3/lineage/trust/TrustAppsActivity.java
@@ -42,6 +42,7 @@ import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.lineage.trust.db.TrustComponent;
+import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
 
 import java.util.List;
 
@@ -56,8 +57,8 @@ public class TrustAppsActivity extends Activity implements
     private LinearLayout mLoadingView;
     private ProgressBar mProgressBar;
 
+    private TrustDatabaseHelper mDbHelper;
     private TrustAppsAdapter mAdapter;
-    private AppLockHelper mAppLockHelper;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstance) {
@@ -75,8 +76,8 @@ public class TrustAppsActivity extends Activity implements
         mProgressBar = findViewById(R.id.hidden_apps_progress_bar);
 
         final boolean hasSecureKeyguard = Utilities.hasSecureKeyguard(this);
-        mAdapter = new TrustAppsAdapter(this, this, hasSecureKeyguard);
-        mAppLockHelper = AppLockHelper.getInstance(this);
+        mAdapter = new TrustAppsAdapter(this, hasSecureKeyguard);
+        mDbHelper = TrustDatabaseHelper.getInstance(this);
 
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
@@ -84,7 +85,7 @@ public class TrustAppsActivity extends Activity implements
 
         showOnBoarding(false);
 
-        new LoadTrustComponentsTask(mAppLockHelper, getPackageManager(), this, this).execute();
+        new LoadTrustComponentsTask(mDbHelper, getPackageManager(), this).execute();
     }
 
     @Override
@@ -110,12 +111,12 @@ public class TrustAppsActivity extends Activity implements
 
     @Override
     public void onHiddenItemChanged(@NonNull TrustComponent component) {
-        new UpdateItemTask(mAppLockHelper, this, HIDDEN).execute(component);
+        new UpdateItemTask(mDbHelper, this, HIDDEN).execute(component);
     }
 
     @Override
     public void onProtectedItemChanged(@NonNull TrustComponent component) {
-        new UpdateItemTask(mAppLockHelper, this, PROTECTED).execute(component);
+        new UpdateItemTask(mDbHelper, this, PROTECTED).execute(component);
     }
 
     @Override
